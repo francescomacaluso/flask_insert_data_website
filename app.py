cc8532bed8bfbc30c11eb1de5880374cc3232759
fra-qlik <francescomacaluso@outlook.com>
2024-01-06 14:19:57 -0500

    remove comments

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from config import db_params  # Import the db_params from the external config mo
 
 app = Flask(__name__)
 
-# Remove the local definition of db_params in this file
-
 @app.route('/')
 def index():
     # Connect to the database
@@ -44,44 +42,6 @@ def submit():
 
         return redirect(url_for('index'))
 
-# @app.route('/update', methods=['POST'])
-# def update():
-#     if request.method == 'POST':
-#         data = request.get_json()
-#         updated_value = data.get('updatedValue')
-#         column_name = data.get('columnName')
-#         record_id = data.get('id')
-
-#         try:
-#             # Whitelist of allowed column names
-#             allowed_columns = ['payment_date', 'birth_date', 'name', 'amount']
-
-#             # Check if the column_name is allowed
-#             if column_name not in allowed_columns:
-#                 return jsonify({'error': 'Invalid column name'})
-
-#             # Connect to the PostgreSQL database using your db_params
-#             conn = psycopg2.connect(**db_params)
-#             cursor = conn.cursor()
-
-#             # Construct the SQL query using a parameterized query
-#             # This ensures that user input is properly escaped and quoted
-#             sql_query = f"UPDATE public_test.payments SET {column_name} = %s WHERE id = %s;"
-
-#             # Execute the query with the updated value and the 'id' as a condition
-#             cursor.execute(sql_query, (updated_value, record_id))
-#             conn.commit()
-
-#             # Close the cursor and connection
-#             cursor.close()
-#             conn.close()
-
-#             return jsonify({'message': f'{column_name} updated successfully'})
-
-#         except Exception as e:
-#             # Handle any database connection or query errors
-#             return jsonify({'error': str(e)})
-
 @app.route('/chart')
 def chart():
     # Connect to the database
@@ -90,20 +50,13 @@ def chart():
 
     # Query the data from the table
     cursor.execute("SELECT name, sum(amount) as amount FROM public_test.payments group by name order by amount desc;")
- #   cursor.execute("SELECT payment_date, sum(amount) as amount FROM public_test.payments group by payment_date order by payment_date asc;")
 
     rows = cursor.fetchall()
-#    rows_line_chart = cursor.fetchall()
 
     chart_data = {
         'name': [row[0] for row in rows],
         'amount': [round(row[1], 2) for row in rows]
     }
-
-#    line_chart_data = {
-#        'payment_date': [row[0] for row in rows_line_chart],
-#        'amount': [round(row[1], 2) for row in rows_line_chart]        
-#    }
 
     # Close the cursor and connection
     cursor.close()
